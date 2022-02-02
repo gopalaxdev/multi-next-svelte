7136a573d1b5ff637c1473bbeb1e45a7d844d73d
gops12345 <agrawalgopala@gmail.com>
2022-02-02 17:02:12 +0530

    added rewrites to shell

--- shell/next.config.js
+++ shell/next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+      {
+        source: "/dashboard",
+        destination: "https://svelte-next-dashboard.vercel.app/",
+      },
+    ];
+  },
 };
