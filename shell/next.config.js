95931ecc6e3e4995b4399aa2d6bc59a78032f699
gops12345 <agrawalgopala@gmail.com>
2022-02-02 16:57:15 +0530

    without rewrites

--- shell/next.config.js
+++ shell/next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/:path*",
-      },
-      {
-        source: "/dashboard",
-        destination: "",
-      },
-    ];
-  },
 };
